d79c668103a6e291d510d88ae9578abbc60a2c20
zakkycrypt01 <zakkycrypt@gmail.com>
2026-01-19 23:05:08 +0100

    feat: Integrate Coinbase Smart Wallet for login, create a wallet synchronization component, and update the login page UI.

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -6,6 +6,7 @@ import { OnchainKitProvider } from '@coinbase/onchainkit';
 import { base } from 'wagmi/chains';
 import { config } from '@/lib/wagmi';
 import { useState } from 'react';
+import { WalletSync } from "@/components/auth/WalletSync";
 
 export function Providers({ children }: { children: React.ReactNode }) {
     const [queryClient] = useState(() => new QueryClient());
@@ -17,6 +18,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
                     chain={base}
                     schemaId="0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9"
                 >
+                    <WalletSync />
                     {children}
                 </OnchainKitProvider>
             </QueryClientProvider>
